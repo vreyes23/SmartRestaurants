8435033a7e2a17a3a87e581b6766705eee6ce11d
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-21 23:17:53 -0700

    Added confirmation activity, now I need to get the buttons that are pressed and pass it to the confirmation activity and get the total of the order. After we can send this data to Pooja’s website.

---
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/Confirmation.java
@@ -0,0 +1,15 @@
+package com.restuarants.smart.speechtotextwatsonv7;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Confirmation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+    }
+}
--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MenuActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MenuActivity.java
@@ -1,14 +1,25 @@
 package com.restuarants.smart.speechtotextwatsonv7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button placeOrderButton = (Button) findViewById(R.id.etPlaceOrder);
+        placeOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Confirmation.class);
+                MenuActivity.this.startActivity(intent);
+            }
+        });
     }
 
     public void onToggleClicked(View view) {
