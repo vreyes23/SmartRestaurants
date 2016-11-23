6ac5496f11b6f61ef06e2f4d62d0634f3b463b3e
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-23 12:40:02 -0700

    Removed unneccassry files and cleaned up the .xml files and added relevent ID’s to the activity_menu.

--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConfirmationActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConfirmationActivity.java
@@ -1,15 +1,16 @@
 package com.restuarants.smart.speechtotextwatsonv7;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-public class Confirmation extends AppCompatActivity {
+public class ConfirmationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("my_variable");
+        }
     }
 }
--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MenuActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MenuActivity.java
@@ -3,7 +3,6 @@ package com.restuarants.smart.speechtotextwatsonv7;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,11 +12,25 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         Button placeOrderButton = (Button) findViewById(R.id.etPlaceOrder);
+        Button double_double_button = (Button) findViewById(R.id.etDouble_double);
+
+        // Button for confirmation
         placeOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MenuActivity.this, Confirmation.class);
+                Intent intent = new Intent(MenuActivity.this, ConfirmationActivity.class);
                 MenuActivity.this.startActivity(intent);
+            }
+        });
+
+        // Button for the Double_Double
+        double_double_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String value = "value";
+                Intent i = new Intent(getApplicationContext(), MenuActivity.class);
+                i.putExtra("my_variable",value);
+                startActivity(i);
             }
         });
     }
