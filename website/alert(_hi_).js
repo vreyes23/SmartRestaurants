42bb6c2cdcf4fed6979a50af66840810c99b96a1
rsburugu <rsburugu@miners.utep.edu>
2016-11-26 15:46:07 -0700

    updated the alert("hi").js

--- website/alert(_hi_).js
+++ website/alert(_hi_).js
@@ -8,14 +8,14 @@ function readFromDB()
 	$.ajax
 	({
 	  type: "GET",
-	  url: "https://833c1344-ce44-4b24-b461-98b8faec0b65-bluemix.cloudant.com/smartrestaurants/_all_docs",
+	  url: "https://85d7a97e-6a4a-4710-bf33-116d883d77bb-bluemix.cloudant.com/test/_all_docs",
 	  dataType: 'json',
 	  async: false,
 	  headers: {
 	  //	"Access-Control-Allow-Origin: ": "*",
       //"Access-Control-Allow-Methods: ": "GET",
       //"Access-Control-Allow-Headers: ": "Authorization",
-	    "Authorization": "Basic " + btoa("thennegencelfinglygodycr" + ":" + "787e88b978422e10f4e8b25318ec8b4c067bb5d3")
+	    "Authorization": "Basic " + btoa("rryoupperarnotedgenerste" + ":" + "c91a8e3515d96e5eeb3319f80d7ef35733413516")
 	  },
 	  //data: '{ "comment" }',
 	  success: function(data) {  
