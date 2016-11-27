db923289f03629943c2f88c34293e05586272afd
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-26 17:34:33 -0700

    no message

---
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/SpeechToText.java
@@ -0,0 +1,8 @@
+package com.restuarants.smart.speechtotextwatsonv7;
+
+/**
+ * Created by oscarricaud on 11/26/16.
+ */
+
+public class SpeechToText {
+}
---
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/TextToSpeech.java
@@ -0,0 +1,80 @@
+package com.restuarants.smart.speechtotextwatsonv7;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class TextToSpeech {
+    private final String WEBSERVICE_URL = "https://stream.watsonplatform.net/text-tospeech/api/v1/synthesize";
+    private final String USERNAME = "your username goes here";
+    private final String PASSWORD = "your password goes here";
+    private String type = "POST";
+    private Context appContext;
+    private File convertedFile;
+    private String inputText;
+    public TextToSpeech(Context appContext) {
+        this.appContext = appContext;
+    }
+    public void execute(String inputText) {
+        this.inputText = inputText;
+        new TheTask().execute(WEBSERVICE_URL);
+    }
+    class TheTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... arg0) {
+            String text = null;
+            try {
+                com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech service = new
+                        com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech();
+                service.setUsernameAndPassword(USERNAME, PASSWORD);
+                try {
+                    text = inputText;
+                    InputStream stream = service.synthesize(text, Voice.EN_ALLISON, "audio/wav");
+                    File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    convertedFile = new File(downloadsFolder, "test.ogg");
+                 //   File.createTempFile("convertedFile", ".ogg", downloadsFolder);
+                    FileOutputStream out = new FileOutputStream(convertedFile);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = stream.read(buffer)) > 0) {
+                        out.write(buffer, 0, length);
+                    }
+                    out.close();
+                    stream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        public void playFile() {
+            try {
+                FileInputStream fis = new FileInputStream(convertedFile);
+                MediaPlayer mp = new MediaPlayer();
+                mp.setDataSource(fis.getFD());
+                mp.prepare();
+                mp.start();
+            } catch (Exception e) {
+                Log.e("MEDIA", e.toString());
+                e.printStackTrace();
+            }
+        }//end playFile
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            playFile();
+        }
+    }
+}
