c64e777b240a93bba67d83b30d587bd29682949d
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-30 20:41:23 -0700

    Moved the loading circle to the bottom center. Once it’s done loading the user’s order is display at the bottom.

--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MainActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/MainActivity.java
@@ -1,15 +1,19 @@
 package com.restuarants.smart.speechtotextwatsonv7;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 
@@ -18,20 +22,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+
 /**
  * This is the first activity that gets launched when you run this program.
  * MainActivity will be split into two different sections.
  * First section is for customers that do not wish to use the feature of Speech to Text aka Order
  * by speaking. Therefore the user's must navigate through the menu @see MenuActivity and order
  * through there.
+ *
  * @author Oscar I. Ricaud
  * @version 1.0 November 23 2017
  */
 public class MainActivity extends AppCompatActivity {
     private String rawText = "";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     public String getRawText() {
         return rawText;
     }
+
     public void setRawText(String rawText) {
         this.rawText = rawText;
     }
@@ -42,22 +55,27 @@ public class MainActivity extends AppCompatActivity {
     private String outputFilePath;
     private boolean isRecording = false;
     int counter = 0;
+    int currentState = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.loadingPanel).setVisibility(View.INVISIBLE);
+        findViewById(R.id.loadingCircle).setVisibility(View.INVISIBLE); // Hide loading circle
         mic = new RecordWavMaster();
         initialCall(); // Call Watson to Welcome the customer
         setUpButtons();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void initialCall(){
+    public void initialCall() {
         String initialText = "Hello welcome to smart restaurants!";
         TextToSpeechActivity tts = new TextToSpeechActivity(getApplicationContext());
         tts.execute(initialText);
     }
+
     private void setUpButtons() {
         // Left top button, order if you're old school.
         Button menuButton = (Button) findViewById(R.id.etMenu);
@@ -74,11 +92,12 @@ public class MainActivity extends AppCompatActivity {
         speakOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
-                if(counter == 0) {
+                if (counter == 0) {
                     System.out.println("counter == 0 ");
                     mic.recordWavStart();
                     //startRecording();
+                    findViewById(R.id.loadingCircle).setVisibility(View.VISIBLE);
+                    findViewById(R.id.textView9).setVisibility(View.INVISIBLE);
                     speakOrderButton.setText("Stop Recording...");
                 }
                 if (counter == 1) {
@@ -88,15 +107,49 @@ public class MainActivity extends AppCompatActivity {
                     //stopRecording();
                     new SpeechToTextTask().execute(""); // Do the magic and convert audio to a string.
                     counter = 0;
-                }
-                else {
+                } else {
                     counter++;
                 }
             }
         });
     }
+
     private String convertRawTextFromJson(String rawText) {
         return rawText;
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
     private class SpeechToTextTask extends AsyncTask<String, Void, String> {
@@ -104,24 +157,17 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             SpeechToText service = new SpeechToText();
             service.setUsernameAndPassword("b535f60e-dae2-4783-9e89-5ecaf85a468c", "UqbobaXOFPLX");
-
-         //    RecognizeOptions options = new RecognizeOptions().contentType("audio/wav");
             try {
-                System.out.println("in do in background"); // Sketchy here.
-                File sdcard = Environment.getExternalStorageDirectory();
-
-               // File file = new File(outputFilePath);
                 SpeechResults speechResults = service.recognize(new File(outputFilePath)).execute();
-
                 setRawText(speechResults.toString());
                 Log.e(LOG_TAG, String.valueOf(speechResults.getResultIndex()));
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return "";
         }
-        protected void onProgressUpdate(Integer... progress) {}
+        protected void onProgressUpdate(Integer... progress) {
+        }
 
         protected void onPostExecute(String result) {
             System.out.println("On Post Execute");
@@ -130,25 +176,35 @@ public class MainActivity extends AppCompatActivity {
             String finalText = convertRawTextFromJson(getRawText());
             try {
                 JSONObject json = new JSONObject(finalText);
-                JSONArray results= json.getJSONArray("results");
+                JSONArray results = json.getJSONArray("results");
 
-                for(int i = 0 ; i < results.length(); i++){
+                for (int i = 0; i < results.length(); i++) {
                     JSONObject jsonas = results.getJSONObject(i);
                     JSONArray results2 = jsonas.getJSONArray("alternatives");
                     String alternatives = jsonas.getString("alternatives");
                     System.out.println("alternatives " + alternatives);
 
-                    for(int j = 0 ; j < results2.length(); j++){
+                    for (int j = 0; j < results2.length(); j++) {
                         JSONObject jsonasb = results2.getJSONObject(i);
                         String transcript = jsonasb.getString("transcript");
                         System.out.println("transcript " + transcript);
                         TextView placeHolderText = (TextView) findViewById(R.id.textView9);
                         placeHolderText.setText(transcript);
-                        findViewById(R.id.loadingPanel).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.loadingCircle).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.textView9).setVisibility(View.VISIBLE); // show text bottom
 
                         // System.out.println("final text " + finalText); // PRINTS IN JSON format
                         TextToSpeechActivity tts = new TextToSpeechActivity(getApplicationContext());
                         tts.execute("Did you say " + transcript);
+
+                        //  Start having a conversation with Watson, if yes open activity.
+                        //  ConversationStart cs = new ConversationStart();
+                  /*     ConversationService service = new ConversationService("2016-11-29");
+                        service.setUsernameAndPassword("2cd79c69-afac-4288-973a-120db0f1efef", "ENFhhydOkd5q");
+                        MessageRequest newMessage = new MessageRequest.Builder().inputText("I am hungry.").build();
+                        String sillyWorkspaceID = "25dfa8a0-0263-471b-8980-317e68c30488";
+                        MessageResponse response = service.message(sillyWorkspaceID, newMessage).execute();
+                       System.out.println("here " + response); */
                     }
                 }
             } catch (JSONException e) {
