ef1b1a71804a5de85c360de1e45d15f6158d0190
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-12-04 15:58:51 -0700

    Oh my jesus, got it to work. So now the user can ask any of the following: “I want a double” or “I want a cheeseburger” etc etc. If the user asks “I want a cheeseburger”, Watson replies with “Alright, I will make sure that your cheeseburger is made with love”.

--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConversationActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConversationActivity.java
@@ -31,11 +31,13 @@ public class ConversationActivity extends AppCompatActivity{
     private String input_from_user = "";
     private String whatToSay="";
     int counter = 0;
+    int counter_two = 0;
     private String rawText = "";
     private static final String LOG_TAG = "StT";
     private RecordWavMaster mic2;
     private String outputFilePath;
     private Map<String,Object> watsonContext;
+    private MessageRequest WatsonMessageRequest;
     public String getRawText() {
         return rawText;
     }
@@ -58,40 +60,56 @@ public class ConversationActivity extends AppCompatActivity{
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_conversation);
+            mic2 = new RecordWavMaster();
             final TextView userInputPlaceholder = (TextView) findViewById(R.id.textView10); // prints user input
             // Receiving total price from @see MenuActivity2
             Bundle extras = getIntent().getExtras();
             String userInput = extras.getString("user_input"); // Look for YOUR KEY, variable you're receiving
             userInputPlaceholder.setText(userInput);
             setInput_from_user(userInput);
-            new ConversationTask().execute("");
+            setUpButtons();
         }
 
-    private void setUpButton() {
-
+    private void setUpButtons() {
         final Button pressButton = (Button) findViewById(R.id.press_button_conversation);
         pressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (counter == 0) {
-                    System.out.println("counter == 0 ");
+                    System.out.println("Button pressed once");
                     mic2.recordWavStart();
                     pressButton.setText("Stop Recording...");
                 }
+                // Start convo with Watson
                 if (counter == 1) {
-                    System.out.println("counter == 1 ");
                     pressButton.setText("Speak Order");
                     outputFilePath = mic2.recordWavStop();
                     //stopRecording();
                     new SpeechToTextTask().execute(""); // Do the magic and convert audio to a string.
                     new ConversationTask().execute("");
-                    mic2.releaseRecord();
                     counter = 0;
-                } else {
+                }
+                else {
                     counter++;
                 }
             }
         });
+    }
+
+    public Map<String, Object> getWatsonContext() {
+        return watsonContext;
+    }
+
+    public void setWatsonContext(Map<String, Object> watsonContext) {
+        this.watsonContext = watsonContext;
+    }
+
+    public MessageRequest getWatsonMessageRequest() {
+        return WatsonMessageRequest;
+    }
+
+    public void setWatsonMessageRequest(MessageRequest watsonMessageRequest) {
+        WatsonMessageRequest = watsonMessageRequest;
     }
 
 
@@ -100,27 +118,30 @@ public class ConversationActivity extends AppCompatActivity{
                 System.out.println("do in background conversation start");
                 final ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_07_11);
                 service.setUsernameAndPassword("2cd79c69-afac-4288-973a-120db0f1efef", "ENFhhydOkd5q");
-
-                // sync
-                MessageRequest newMessage = new MessageRequest.Builder().inputText(getInput_from_user()).build();
                 String orderWorkspaceID = "ce288cad-6cd5-450f-8225-01216386761e";
-                MessageResponse response = service.message(orderWorkspaceID, newMessage).execute();
-                Map<String, Object> context = response.getContext();
-                System.out.println("context " + context);
+                // If context is empty, other words is this the first time watson converses with user?
 
-                MessageRequest newMessage2 = new MessageRequest.Builder().inputText("yes").context(context).build();
-                MessageResponse response2 = service.message(orderWorkspaceID, newMessage2).execute();
-                System.out.println("response2 " + response2);
-                Map<String, Object> context2 = response2.getContext();
-                System.out.println("context2 " + context2);
-
-/*
-                MessageRequest newMessage2 = new MessageRequest.Builder().inputText( "yes").build();
-                MessageResponse response2 = service.message(orderWorkspaceID, newMessage2).execute();
-                System.out.println("response2 " + response2);
-*/
+                if(getWatsonContext() == null){
+                    System.out.println("Context is empty therefore start a conversation with Watson. ");
+                    MessageRequest newMessage = new MessageRequest.Builder().inputText(getInput_from_user()).build();
+                    MessageResponse response = service.message(orderWorkspaceID, newMessage).execute();
+                    Map<String, Object> context = response.getContext();
+                    System.out.println("context " + context);
+                    setWatsonContext(context);
+                    setWatsonMessageRequest(newMessage);
+                }
+                else{
+                        System.out.println("Context is not empty therefore continue conversation with user ");
+                        MessageRequest newMessage2 = new MessageRequest.Builder().inputText(getInput_from_user()).context(getWatsonContext()).build();
+                        MessageResponse response2 = service.message(orderWorkspaceID, newMessage2).execute();
+                        System.out.println("response2 " + response2);
+                        Map<String, Object> context2 = response2.getContext();
+                        System.out.println("context2 " + context2);
+                        setWatsonContext(context2);
+                        setWatsonMessageRequest(newMessage2);
+                }
                 // rx async callback
-                service.message(orderWorkspaceID, newMessage).rx().thenApplyAsync(new CompletableFuture.Fun<MessageResponse, Map<String, Object>>() {
+                service.message(orderWorkspaceID, getWatsonMessageRequest()).rx().thenApplyAsync(new CompletableFuture.Fun<MessageResponse, Map<String, Object>>() {
                     @Override
                     public Map<String, Object> apply(MessageResponse message) {
                         System.out.println("Context FOUND" + message.getContext());
@@ -135,26 +156,25 @@ public class ConversationActivity extends AppCompatActivity{
                                 TextToSpeechActivity tts = new TextToSpeechActivity(getApplicationContext());
                                 tts.execute("" + entry.getValue());
                                 setInput_from_user("yes");
-                                //new ConversationTask().execute("");
-
                             }
                         }
-
                     }
                 });
                 MessageResponse rxMessageResponse = null;
                 try {
-                    rxMessageResponse = service.message(orderWorkspaceID, newMessage).rx().get();
+                    rxMessageResponse = service.message(orderWorkspaceID, getWatsonMessageRequest()).rx().get();
                     System.out.println("rxRESPONSE" + rxMessageResponse);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
+                } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
                 }
-
                 return null;
             }
+        protected void onPostExecute(String result) {
+            System.out.println("ON POST EXECUTE CONVO");
+
         }
+    }
+
     public class SpeechToTextTask extends AsyncTask<String, Void, String> {
         protected String doInBackground(String... params) {
             SpeechToText service = new SpeechToText();
@@ -188,7 +208,6 @@ public class ConversationActivity extends AppCompatActivity{
                         String transcript = jsonasb.getString("transcript");
                         System.out.println("transcript " + transcript);
                         setInput_from_user(transcript);
-
                     }
                 }
             } catch (JSONException e) {
