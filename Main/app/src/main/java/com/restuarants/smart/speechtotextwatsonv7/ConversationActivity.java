03e6e1268fe07ab808378964d0d0e0c8ce8883fa
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-12-04 20:38:59 -0700

    Added Javadoc so you guys can understand the process on how I resolve the issue I had yesterday.

--- Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConversationActivity.java
+++ Main/app/src/main/java/com/restuarants/smart/speechtotextwatsonv7/ConversationActivity.java
@@ -24,7 +24,13 @@ import java.util.concurrent.ExecutionException;
 
 import jersey.repackaged.jsr166e.CompletableFuture;
 /**
+ * This class allows the user to interact with watson. It first waits for the user to press the button
+ * and once this button is pressed and the RecordWavMaster starts recording the user audio. If the
+ * button is pressed again it stops recording and converts this audio to a string. Then @see ConversationTask
+ * looks for a corresponding pre-defined intent of the string that was obtained earlier to converse with
+ * the user.
  * @author Oscar I. Ricaud
+ * @version 1.0.0 December 04 2016
  */
 public class ConversationActivity extends AppCompatActivity{
     private String input_from_user = "";
@@ -67,6 +73,14 @@ public class ConversationActivity extends AppCompatActivity{
             setUpButtons();
         }
 
+    /**
+     * This is method the most important section of this class because it allows the user to send
+     * multiple instances with Watson Conversation. When the button is pressed it records
+     * audio. If the user presses the button again it stops recording and tells the @see SpeechToTestTask
+     * to convert this audio to a String. Then @see ConversationTask looks for the corresponding intent
+     * based on the specific String that was obtained at @see SpeechToTestTask. In conclusion the button
+     * allows for the user to keep interacting with watson.
+     */
     private void setUpButtons() {
         final Button pressButton = (Button) findViewById(R.id.press_button_conversation);
         pressButton.setOnClickListener(new View.OnClickListener() {
@@ -108,34 +122,45 @@ public class ConversationActivity extends AppCompatActivity{
     public void setWatsonMessageRequest(MessageRequest watsonMessageRequest) {
         WatsonMessageRequest = watsonMessageRequest;
     }
-
-
+    /**
+     * This class calls the front end conversation tree I created on IBM's website. The conversation
+     * starts at the root and waits for the user input to find the corresponding intent that has
+     * been already defined. A scenario could be In-N-Out customers could say the following:
+     * "I would want to order a double double." Watson immediately finds the intent, #double_double
+     * and repeats what the intent has defined. In this case Watson would replied with "The double
+     * double includes lettuce, tomato, spread with or without onions and stacked high on a freshly
+     * baked bun. Is this what you would like to order?" The user could reply with Yes or No and repeats
+     * the process, This class looks for the corresponding intent.
+     * @author Oscar I. Ricaud
+     * @version 1.0.0 December 05 2016
+     */
     private class ConversationTask extends AsyncTask<String, Void, String> {
             protected String doInBackground(String... strings) {
                 System.out.println("do in background conversation start");
                 final ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_07_11);
                 service.setUsernameAndPassword("2cd79c69-afac-4288-973a-120db0f1efef", "ENFhhydOkd5q");
                 String orderWorkspaceID = "ce288cad-6cd5-450f-8225-01216386761e";
-                // If context is empty, other words is this the first time watson converses with user?
 
+                // If context is empty, where context means if there has been an instance of the user
+                // interacting with Watson
                 if(getWatsonContext() == null){
-                    System.out.println("Context is empty therefore start a conversation with Watson. ");
+                    System.out.println("Context is empty therefore start a new conversation with Watson. ");
                     MessageRequest newMessage = new MessageRequest.Builder().inputText(getInput_from_user()).build();
                     MessageResponse response = service.message(orderWorkspaceID, newMessage).execute();
                     Map<String, Object> context = response.getContext();
                     System.out.println("context " + context);
                     setWatsonContext(context);
                     setWatsonMessageRequest(newMessage);
-                }
-                else{
+                } else {  // There has been an instance with the user interacting with Watson, continue
+                        // Conversation with user.
                         System.out.println("Context is not empty therefore continue conversation with user ");
-                        MessageRequest newMessage2 = new MessageRequest.Builder().inputText(getInput_from_user()).context(getWatsonContext()).build();
-                        MessageResponse response2 = service.message(orderWorkspaceID, newMessage2).execute();
-                        System.out.println("response2 " + response2);
-                        Map<String, Object> context2 = response2.getContext();
-                        System.out.println("context2 " + context2);
-                        setWatsonContext(context2);
-                        setWatsonMessageRequest(newMessage2);
+                        MessageRequest messageContext = new MessageRequest.Builder().inputText(getInput_from_user()).context(getWatsonContext()).build();
+                        MessageResponse messageContextResponse = service.message(orderWorkspaceID, messageContext).execute();
+                        System.out.println("messageContextResponse " + messageContextResponse);
+                        Map<String, Object> curr_context = messageContextResponse.getContext();
+                        System.out.println("curr_context " + curr_context);
+                        setWatsonContext(curr_context);
+                        setWatsonMessageRequest(messageContext);
                 }
                 // rx async callback
                 service.message(orderWorkspaceID, getWatsonMessageRequest()).rx().thenApplyAsync(new CompletableFuture.Fun<MessageResponse, Map<String, Object>>() {
@@ -171,7 +196,12 @@ public class ConversationActivity extends AppCompatActivity{
 
         }
     }
-
+    /**
+     * This class verifies the services username and password and allows to take into the argument
+     * an Audio file and converts this audio file into text.
+     * @author Oscar I. Ricaud
+     * @version 1.0.0 12/04/2016
+     */
     public class SpeechToTextTask extends AsyncTask<String, Void, String> {
         protected String doInBackground(String... params) {
             SpeechToText service = new SpeechToText();
